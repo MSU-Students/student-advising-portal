749128c6dab2fa465bf007172871624e278f49db
fencim <fencim@gmail.com>
2024-11-09 06:38:05 +0800

    setup db connection

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -1,14 +1,15 @@
 
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-  measurementId: ''
+  // databaseURL: '',
+  apiKey: 'AIzaSyAnTpKMfEF1aNHOoiGbqNHTdL2ihKr7jDk',
+  authDomain: 'gonana-o.firebaseapp.com',
+  projectId: 'gonana-o',
+  storageBucket: 'gonana-o.firebasestorage.app',
+  messagingSenderId: '301350859500',
+  appId: '1:301350859500:web:b20ddc5d5860ebb9c3abc9',
+  measurementId: 'G-ZB0G111937'
 };
+
 
 const firebaseConfigDev = firebaseConfig;
 module.exports = {
--- src/services/firebase.collections.ts
+++ src/services/firebase.collections.ts
@@ -3,5 +3,6 @@ import { Firestore, collection } from 'firebase/firestore';
 export function getCollections(fbStore: Firestore) {
   return {
     'profiles': () => collection(fbStore, 'profiles'),
+    'bookings': () => collection(fbStore, 'bookings'),
   };
 }
