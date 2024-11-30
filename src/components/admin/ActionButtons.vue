9eab29ea2ef42b24ccdfb1292e019ad99ae65fc1
Mangorangca, Abdul Hamid <lavahotsnacks@gmail.com>
2024-11-30 22:50:40 +0800

    refactor admin dashboard into components

---
+++ src/components/admin/ActionButtons.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-td :props="props" class="text-right">
+    <q-btn color="green" class="q-mx-sm">Approve</q-btn>
+    <q-btn color="red" class="q-mx-sm">Reject</q-btn>
+  </q-td>
+</template>
+
+<script setup>
+defineProps({
+  props: Object,
+});
+</script>
---
+++ src/components/admin/FilterDropdown.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-btn-dropdown label="Filter" color="primary" class="q-ml-sm">
+    <q-list>
+      <q-item clickable v-ripple>
+        <q-item-section>Approved</q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>Denied</q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>Pending</q-item-section>
+      </q-item>
+    </q-list>
+  </q-btn-dropdown>
+</template>
+
+<script setup></script>
