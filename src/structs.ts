d17f3c977d9ac5b3faaf08cff28a0edf66160bd8
fencim <fencim@gmail.com>
2024-12-17 06:19:34 +0800

    Allow Promised cb

--- src/structs.ts
+++ src/structs.ts
@@ -9,7 +9,7 @@ export type EmitStruct<T extends { type: string; arg: A }, A = T['arg']> = {
   loggable?: 'operation' | 'post-operation' | 'query';
   module?: string;
   icon?: string;
-  cb: (e: A) => void;
+  cb: ((e: A) => void) | ((e: A) => Promise<void>);
 };
 
 export type ToEmit<T extends { type: string; arg: any }, U> = T extends U
