3c1ab3cb423d877e6e7a3300a73531dec05657a0
29-29 <ackx29sodo@gmail.com>
2024-12-20 23:22:59 +0800

    implemented booking cancellation

---
+++ src/workflows/booking/cancel-booking.workflow.ts
@@ -0,0 +1,23 @@
+import { useBookingStore } from 'src/stores/booking.store';
+import { TheWorkflows } from '../the-workflows';
+
+TheWorkflows.on({
+  type: 'cancelBooking',
+  async cb(e) {
+    try {
+      if (!e.payload.booking.key) return;
+      const bookingStore = useBookingStore();
+
+      const booking = await bookingStore.getBooking(e.payload.booking.key);
+      if (!booking) return;
+      await bookingStore.cancelBooking(e.payload.booking);
+      e.success &&
+        e.success({
+          ...e.payload.booking,
+          status: 'cancelled',
+        });
+    } catch (error) {
+      e.error?.(new Error(String(error)));
+    }
+  },
+});
--- src/workflows/booking/index.ts
+++ src/workflows/booking/index.ts
@@ -1,3 +1,4 @@
 import './book-appointment.workflow';
 import './reject-consultation.workflow';
 import './update-booking.workflow';
+import './cancel-booking.workflow';
